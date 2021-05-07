8a7ece65b366e23ab760b818fbabafed8c4672e5
Andrew Woods <services@awoods.net>
2021-05-07 16:41:36 -0400

    Replace variables with symbolic constants
    Just following along with the examples. I kinda think these values
    wouldn't normally be constants, as I want to pass them in from the
    command line - but we're not there yet! So just trying it out :)

--- k-and-r/ch-01/temperature.c
+++ k-and-r/ch-01/temperature.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
+#define LOWER 0 // The lower limit of the table
+#define UPPER 300 // The upper limit of the table
+#define STEP 20 // The value to increment
+
 /*
  * Produce a table of temperatures converting Fahrenheit to Celsius
  */
 main() {
     int fahr;
     float celsius;
-    int lower, upper, step;
 
-    lower = 0;
-    upper = 300;
-    step = 20;
-
-    fahr = lower;
-    while(fahr < upper) {
+    fahr = LOWER;
+    while(fahr < UPPER) {
         celsius = (5.0 / 9.0) * (fahr - 32);
         printf("%3d\t%6.1f\n", fahr, celsius);
-        fahr += step;
+        fahr += STEP;
     }
 }
