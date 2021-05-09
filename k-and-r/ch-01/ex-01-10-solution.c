480bffa5ac40a7dc60c6a4b2a3c6d626f6fd0003
Andrew Woods <services@awoods.net>
2021-05-09 17:47:35 -0400

    Add solutions for exercise 1-10

---
+++ k-and-r/ch-01/ex-01-10-solution.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* 
+ * Prefix special characters with a \ to display it literally 
+ */
+main() {
+    int c; 
+
+    while ((c = getchar()) != EOF) {
+        if (c == '\t') {
+            putchar('\\');
+            putchar('t');
+        } else if (c == '\b') {
+            putchar('\\');
+            putchar('b');
+        } else if (c == '\\') {
+            putchar('\\');
+            putchar('\\');
+        } else {
+            putchar(c);
+        }
+    }
+
+}
